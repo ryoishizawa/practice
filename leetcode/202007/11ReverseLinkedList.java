036c8bb4451ccb479195890a31872e4d8c2b7bb8
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-07-11 10:51:53 +0800

    Create 11ReverseLinkedList.java

---
+++ leetcode/202007/11ReverseLinkedList.java
@@ -0,0 +1,24 @@
+// Copied from leetcode
+// Good explanation in YouTube: https://www.youtube.com/watch?v=MRe3UsRadKw&feature=emb_logo
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution1 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
