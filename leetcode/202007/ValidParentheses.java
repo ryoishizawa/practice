ba3717d1899d5525a30634f3092a03cc45329b35
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-07-16 22:03:30 +0800

    Update ValidParentheses.java

--- leetcode/202007/ValidParentheses.java
+++ leetcode/202007/ValidParentheses.java
@@ -32,3 +32,25 @@ class Solution {
         return false;
     }
 }
+
+// improved using LeetCode solution
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (stack.empty() || stack.pop() != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+}
