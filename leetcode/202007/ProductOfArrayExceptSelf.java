14d04991472cc982719c808a3ff5731980ff5f16
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-07-17 23:52:13 +0800

    Update ProductOfArrayExceptSelf.java

--- leetcode/202007/ProductOfArrayExceptSelf.java
+++ leetcode/202007/ProductOfArrayExceptSelf.java
@@ -1,6 +1,6 @@
 // Date: 17/07/2020
 // Accepted after reading the article in LeetCode.
-class Solution {
+class Solution1 {
     public int[] productExceptSelf(int[] nums) {
         int[] leftProduct = new int[nums.length];
         int[] rightProduct = new int[nums.length];
@@ -27,6 +27,37 @@ class Solution {
                 result[i] = leftProduct[i - 1] * rightProduct[i + 1];
             }
         }
+        return result;
+    }
+}
+
+
+// Solved with O(1) space complexity.
+// But this is a bit slower even though time complexity is the same.
+class Solution2 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        
+        int leftSum = 1;
+        for (int i = 0; i < nums.length; i++) {
+            leftSum = leftSum * nums[i];
+            result[i] = leftSum;
+        }
+        
+        int rightSum = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                result[i] = rightSum;
+                continue;
+            }
+            if (i == nums.length - 1) { 
+                result[i] = result[i - 1];
+            } else {
+                result[i] = result[i - 1] * rightSum;
+            }
+            rightSum = rightSum * nums[i];
+        }
+        
         return result;
     }
 }
