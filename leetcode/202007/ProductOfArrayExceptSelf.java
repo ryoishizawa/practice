c7a8a165d4bf279d359d89da90b1dc06cc4404e0
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-07-17 23:24:43 +0800

    Update ProductOfArrayExceptSelf.java

--- leetcode/202007/ProductOfArrayExceptSelf.java
+++ leetcode/202007/ProductOfArrayExceptSelf.java
@@ -1,26 +1,31 @@
 // Date: 17/07/2020
-
-// wrong answer
+// Accepted after reading the article in LeetCode.
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        List<Integer> leftProduct = new ArrayList<>();
-        List<Integer> rightProduct = new ArrayList<>();
+        int[] leftProduct = new int[nums.length];
+        int[] rightProduct = new int[nums.length];
         
         int leftSum = 1;
         for (int i = 0; i < nums.length; i++) {
             leftSum = leftSum * nums[i];
-            leftProduct.add(leftSum);
+            leftProduct[i] = leftSum;
         }
         
         int rightSum = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
             rightSum = rightSum * nums[i];
-            rightProduct.add(rightSum);
+            rightProduct[i] = rightSum;
         }
         
         int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            result[i] = leftProduct.get(i) * rightProduct.get(i);
+            if (i == 0) {
+                result[i] = rightProduct[i + 1];
+            } else if (i == nums.length - 1) {
+                result[i] = leftProduct[i - 1];
+            } else {
+                result[i] = leftProduct[i - 1] * rightProduct[i + 1];
+            }
         }
         return result;
     }
