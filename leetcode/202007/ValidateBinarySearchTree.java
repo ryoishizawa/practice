92b82c6aafb598e002306488967a5b001f99c3eb
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-07-18 15:44:46 +0800

    Update ValidateBinarySearchTree.java
    hard to solve..

--- leetcode/202007/ValidateBinarySearchTree.java
+++ leetcode/202007/ValidateBinarySearchTree.java
@@ -16,24 +16,9 @@ class Solution {
         if (higher != null && root.val >= higher) {
             return false;
         }
-        // check root node
-        if (root.left != null) {
-            if (root.val <= root.left.val) {
-                return false;
-            }
-        }
-        if (root.right != null) {
-            if (root.val >= root.right.val) {
-                return false;
-            }
-        }
-        // check right sub tree
-        if (lower == null) lower = root.val;
-        boolean isLeftValid = helper(root.left, lower, null);
-        // check left tree
-        if (higher == null) higher = root.val;
-        boolean isRightValid = helper(root.right, null, higher);
-        // if one of them are false, return false. Otherwise, return true
+
+        boolean isLeftValid = helper(root.left, lower, root.val);
+        boolean isRightValid = helper(root.right, root.val, higher);
         return isLeftValid && isRightValid;
     }
 }
